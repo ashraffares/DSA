f0d804fc2140e301d6c8e1769beec11e003bd804
ashraffares <ashraffares090@gmail.com>
2021-10-03 13:04:32 +0200

    Add add and remove methods for queue using linkedlist

---
+++ DS/queue/challenge.rb
@@ -0,0 +1,66 @@
+# Start with your code from LinkedList challenge.
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class Queue
+  @head = nil
+  @tail = nil
+  def add(number)
+    # your code here
+    if @head.nil?
+      new_node = Node.new(number)
+      @head = new_node
+      @tail = new_node
+      @head.value
+    else
+      temp = @tail
+      @tail = Node.new(number)
+      temp.next_node = @tail
+      @tail.value
+    end
+  end
+
+  def remove
+    # your code here
+    return -1 if @head.nil?
+
+    if @head.next_node.nil?
+      temp = @head
+      @head = nil
+      temp.next_node = nil
+      return temp.value
+    end
+
+    temp = @head
+    @head = temp.next_node
+    temp.next_node = nil
+    temp.value
+  end
+end
+
+queue = Queue.new
+queue.add(3)
+queue.add(5)
+puts queue.remove
+# => 3
+
+queue.add(2)
+queue.add(7)
+puts queue.remove
+# => 5
+
+puts queue.remove
+# => 2
+
+puts queue.remove
+# => 7
+
+puts queue.remove
+# => -1
