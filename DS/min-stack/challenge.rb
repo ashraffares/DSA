82d8ea7a07e10a35bef71e1dd0559018da22d776
ashraffares <ashraffares090@gmail.com>
2021-10-04 11:51:13 +0200

    Add min method to stack

---
+++ DS/min-stack/challenge.rb
@@ -0,0 +1,66 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class Stack
+  @head = nil
+  @headmin = nil
+  def push(number)
+    # your code here
+    if @head.nil?
+      @head = Node.new(number)
+      @headmin = Node.new(number)
+    else
+      temp = @head
+      @head = Node.new(number, temp)
+      if @headmin.value > @head.value
+        @headmin = Node.new(number, temp)
+      end
+    end
+  end
+
+  def pop
+    # your code here
+    return false if @head.nil?
+
+    if @headmin.value == @head.value
+      temp = @headmin
+      @headmin = temp.next_node
+      temp.next_node = nil
+      temp.value
+    end
+    temp = @head
+    @head = temp.next_node
+    temp.next_node = nil
+    temp.value
+  end
+
+  def min
+    @headmin.value
+  end
+end
+
+stack = Stack.new
+
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+# => 3
