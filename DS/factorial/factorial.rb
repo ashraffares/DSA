dcaf87d12d965cb80877f8c9451c6357f424237c
ashraffares <ashraffares090@gmail.com>
2021-10-07 10:53:13 +0200

    factorial recursive

---
+++ DS/factorial/factorial.rb
@@ -0,0 +1,18 @@
+puts 'Find factorial of: '
+
+factorial = gets
+factorial = factorial.to_i
+until factorial
+    puts 'Find factorial of: '
+    factorial = gets
+end
+
+def fact(num)
+  if num.zero?
+    1
+  else
+    num * fact(num - 1)
+  end
+end
+
+puts fact(factorial)
