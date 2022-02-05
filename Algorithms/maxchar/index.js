085c217deacac7b47a55a82a55c5aa0a12a7abf4
ashraffares <ashraffares090@gmail.com>
2022-02-05 13:40:33 +0200

    add maxchar-reverseint-fuzzbuzz

---
+++ Algorithms/maxchar/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    const chars = {};
+
+    for (let char of str) {
+        chars[char] = chars[char] + 1 || 1;
+        // if (!chars[char]) {
+        //     chars[char] = 1;
+        // }
+    
+        // chars[char]++;
+    }
+    
+    
+    let max = 0;
+    let maxcharval = '';
+    for (key in chars) {
+        if (chars[key] > max) {
+            max = chars[key]
+            maxcharval = key
+        }
+    }
+
+    return maxcharval;
+}
+
+
+
+module.exports = maxChar; 
